2d75d9fdcb3adbaec4f5130849d817d6dd12ebeb
Cody <kovarcody@gmail.com>
2020-05-21 17:36:30 -0500

    Initial Upload of Project

---
+++ cynked.py
@@ -0,0 +1,29 @@
+#mainSync.py
+import subprocess
+import os
+import glob
+import toml
+import glob
+import sys
+
+config_server = toml.loads(open("server.toml", "r").read())
+config_home = toml.loads(open("home.toml", "r").read())
+
+try:
+    pull = sys.argv[1] == "pull"
+    push = sys.argv[1] == "push"
+except IndexError:
+    print("Error: Please enter the correct number of commands (either push or pull)")
+except:
+    print("Error: Uh-oh, not sure what happened here. If error persists, please contact the creator.")
+
+overwrite = True
+
+if pull:
+    subprocess.call(["ssh", config_server["user"] + "@" + config_server["ip"], "cd " + config_server["ks"] + "&& python3 cynked.py push"])
+elif push:
+    for file in glob.glob(str(config_home["file"]) + "*.*"):
+        print("Writing " + file[file.rindex("/") + 1:])
+        subprocess.call(["scp",file,config_server["user"] + "@" + config_server["ip"] + ":" + config_server["file"]])
+else:
+    print("Error: Please enter a valid command in the command line (either push or pull)")
